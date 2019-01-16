6a2391f05461374bee71d1a7f27b5d80ab0c9321
Julian Fletcher <225834@shakopeeschools.org>
2019-01-16 13:31:51 -0600

    Code Change/description stuff
    Added my strategy name and description, and added another if variable to change the way the code is to make it work properly.

--- team23.py
+++ team23.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Julian Fletchers Team' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Possible Partners'
+strategy_description = 'If the last item in their history is \'c\' then collude unless it colluded the last round'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,15 +25,11 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
 
-    if their_history == 'c':
-        return 'c'
+    if their_history[-1] == 'c':
+        if my_score[-1] != 'c':
+            return 'b'  
     else: 
         return 'b'
-        
-        
-    
-    
-
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
