be2133b666fac0b15b36e9a59294d83aefd7009f
Blake Swanson <225774@c1mww5vcj1wk.isd720.com>
2019-01-17 10:21:40 -0600

    Update team11.py
    Updated team stragety, team name, and fixed the line 67 print error.

--- team11.py
+++ team11.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Blake Swanson Team' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
@@ -26,7 +26,18 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)== 0:
+        return 'c'
+    if 'b' in their_history:
+        total = 0
+        for i in their_history:
+            if i == 'b':
+                total += 1
+        if total >2:
+            return 'b'
+        else:
+            return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -53,7 +64,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
