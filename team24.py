adfc7524a7b96dd4d0bc43ee33f0f461d5dfe0ba
Ethan Pearson <Epearson612@Hotmail.com>
2019-01-17 10:05:21 -0600

    Update team24.py
    Added my program function

--- team24.py
+++ team24.py
@@ -1,3 +1,5 @@
+import random
+
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -26,8 +28,25 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    choice = rand.randint(1,3)
+    
+    if choice = 1
+        
+        return 'c'
+    
+    if choice = 2
+    
+        return 'b'
+        
+    if choice = 3
+    
+        if thier_history[-1] == 'c'
+        
+            return 'c'
+        
+        else
+        
+            return 'd'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
