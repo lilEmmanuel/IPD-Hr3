4f23b36c4d2c5d772485cadafbbf25d6601575b9
loganbr316890589754 <46534895+loganbr316890589754@users.noreply.github.com>
2019-01-22 10:31:44 -0600

    Revert "Updated strat"

--- team25.py
+++ team25.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-Team_name = 'Logan Bristols team' # Only 10 chars displayed.
-strategy_name = 'collude until betray'
-strategy_description = 'Collude until they betray and then betray until they collude'
+team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+strategy_name = 'The name the team gives to this strategy'
+strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,12 +25,9 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    if len(my_history) ==0:
-        return 'c'
-    if 'b' in their_history:
-        return 'b'
-    else:
-        return 'c'
+    
+    return 'c'
+
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
@@ -56,7 +53,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print ('Test passed')
+         print 'Test passed'
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
