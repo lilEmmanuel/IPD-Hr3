619bbbfb4e7195c697ccdae7d15e7661b600c787
224928 <224928@shakopeeschools.org>
2019-01-22 10:05:41 -0600

    Update team1.py

--- team1.py
+++ team1.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Logan Corys Team' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Collude until betrayed'
+strategy_description = 'My code colludes until the other player betrays.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
