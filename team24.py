441d4fb6677fccbbe9ed92629e82078d6eb25f2e
Ethan Pearson <Epearson612@Hotmail.com>
2019-01-22 10:16:51 -0600

    Changed my team name and description

--- team24.py
+++ team24.py
@@ -8,8 +8,8 @@ import random
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Ethan Pearson Team' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Team Ethan' # Only 10 chars displayed.
+strategy_name = 'Random int decides my choice'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
